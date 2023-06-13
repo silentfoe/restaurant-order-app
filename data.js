32cd4f31c4a6137bb48dbd958dd62fa797c960e9
Spencer Pingitore <silentfoe6@gmail.com>
2023-06-12 23:42:54 -0400

    adding all files to github. HTML and JS is all done. Need to work on the CSS portion of this project. All functions work as expected for this app currently.

---
+++ data.js
@@ -0,0 +1,45 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 10,
+        emoji: "🍕",
+        id: 0
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+        {
+        name: "Fried Chicken Meal",
+        ingredients: ["breading", "chicken", "potatoes" ],
+        price: 15,
+        emoji: "🍗",
+        id: 2
+    },
+    {
+        name: "Soda",
+        ingredients: [ "corn syrup", "water"],
+        price: 3,
+        emoji: "🥤",
+        id: 3
+    },
+    {
+        name: "Beer",
+        ingredients: ["grain", "hops", "yeast", "water"],
+        price: 12,
+        emoji: "🍺",
+        id: 4
+    },
+    {
+        name: "Bottled Water",
+        ingredients: ["water"],
+        price: 2,
+        emoji: "💧",
+        id: 5
+    }
+]
+
